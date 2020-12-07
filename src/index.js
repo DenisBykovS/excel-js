36ffbba3f4bb675c322f82e2c5587583635a6099
DenisBykovS <47249500+DenisBykovS@users.noreply.github.com>
2020-12-07 12:13:01 +0300

    finish baseframework functional (#3)

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
-
 import './scss/index.scss'
+import {Excel} from '@/components/excel/Excel';
+import {Header} from '@/components/header/Header';
+import {Table} from '@/components/table/Table';
+import {Formula} from '@/components/formula/Formula';
+import {Toolbar} from '@/components/toolbar/Toolbar'
 
-console.log('hello')
+
+const excel = new Excel('#app', {
+  components: [Header, Toolbar, Formula, Table]
+})
+
+excel.render()
